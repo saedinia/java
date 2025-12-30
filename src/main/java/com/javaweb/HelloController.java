6a93978f7534a5fc51ca8da3ec731182b5fee1d6
Sadegh Saedi Nia <saedinia@d-i-s.de>
2025-12-30 17:58:31 +0100

    Refactor project structure and update configurations for Spring Boot application

--- src/main/java/com/javaweb/HelloController.java
+++ src/main/java/com/javaweb/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.myjavaweb;
+package com.javaweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/javaweb/PageController.java
+++ src/main/java/com/javaweb/PageController.java
@@ -1,4 +1,4 @@
-package com.example.myjavaweb;
+package com.javaweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
