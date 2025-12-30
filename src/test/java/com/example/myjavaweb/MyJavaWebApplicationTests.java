0a2e34ddba23173cf4e7b90227c91fbce239ed61
d-i-s-support <support@d-i-s.de>
2025-12-31 00:33:32 +0100

    Fix Spring Boot test configuration for Java 21 upgrade

--- src/test/java/com/example/myjavaweb/MyJavaWebApplicationTests.java
+++ src/test/java/com/example/myjavaweb/MyJavaWebApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.myjavaweb;
 
 import org.junit.jupiter.api.Test;
+import com.javaweb.MyJavaWebApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MyJavaWebApplication.class)
 class MyJavaWebApplicationTests {
 
 	@Test
