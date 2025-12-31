9c3081e6e8cb3b7f2b4c5b4cfd2e2ccaead8f82e
d-i-s-support <support@d-i-s.de>
2025-12-31 01:02:06 +0100

    Update DDEV configuration for JDK 21 and adjust test imports

--- src/test/java/com/example/myjavaweb/MyJavaWebApplicationTests.java
+++ src/test/java/com/example/myjavaweb/MyJavaWebApplicationTests.java
@@ -1,8 +1,9 @@
 package com.example.myjavaweb;
 
 import org.junit.jupiter.api.Test;
-import com.javaweb.MyJavaWebApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.javaweb.MyJavaWebApplication;
 
 @SpringBootTest(classes = MyJavaWebApplication.class)
 class MyJavaWebApplicationTests {
